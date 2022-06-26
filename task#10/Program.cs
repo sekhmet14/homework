e38e6e9412f16cbc1471546458bef0f206d17a4a
sekhmet14 <104780086+sekhmet14@users.noreply.github.com>
2022-06-26 23:17:36 +0700

    задача №10

---
+++ task#10/Program.cs
@@ -0,0 +1,12 @@
+﻿int SnowDigit()
+{
+Console.Write("Введите трехзначное число:");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int n1 = numbers % 100;
+int result = n1 / 10;
+return result;
+
+}
+
+int res = SnowDigit();
+Console.WriteLine(res);
