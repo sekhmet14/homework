f913edc3b67f964b08a430fce457f6992b01240c
sekhmet14 <104780086+sekhmet14@users.noreply.github.com>
2022-06-28 23:24:43 +0700

    задача №13

---
+++ task#13/Program.cs
@@ -0,0 +1,17 @@
+﻿int numbers;
+
+Console.Write("imput numbers:");
+numbers = Convert.ToInt32(Console.ReadLine());
+string numbersline = Convert.ToString(numbers);
+
+if (numbersline.Length > 2)
+{
+
+    Console.WriteLine($"третья цифра : {numbersline[2]}");
+
+}
+else {
+
+    Console.WriteLine("третьей цифры нет!");
+
+}
