63ad815a6d129f9f0e6dc453012e1a69726f5f66
sekhmet14 <104780086+sekhmet14@users.noreply.github.com>
2022-06-28 23:25:51 +0700

    задача №15

---
+++ task#15/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("введите цифру дня недели:");
+
+int daynum = Convert.ToInt32(Console.ReadLine());
+
+void CheckDays(int daynum){
+
+    if (daynum == 6 || daynum == 7) {
+
+        Console.WriteLine($"{daynum} -> да ");
+
+    }
+    else if (daynum < 1 || daynum > 7) {
+
+
+        Console.WriteLine("данного дня не существует в недели!");
+
+    }
+
+    else {
+
+        Console.WriteLine($" {daynum} -> нет");
+
+    }
+
+}
+
+CheckDays(daynum);
