ff79a456b9cc5bbd58c82ecb7c06c6cdd0257950
sekhmet14 <104780086+sekhmet14@users.noreply.github.com>
2022-06-30 23:29:24 +0700

    Задача №19

---
+++ task#19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите 5-ти значное число");
+string? Numbers = Console.ReadLine();
+
+void checkDay(string Numbers)
+{
+
+    if (Numbers[0] == Numbers[4] || Numbers[1] == Numbers[3])
+    {
+
+        Console.WriteLine($"{Numbers} -> да");
+
+    }
+    else
+    {
+
+        Console.WriteLine($"{Numbers} -> нет");
+    }
+}
+checkDay(Numbers!);
+
