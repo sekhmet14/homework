b680f2f0707c66c00455b694938e15dc8621a6ea
sekhmet14 <104780086+sekhmet14@users.noreply.github.com>
2022-07-01 23:24:05 +0700

    задача №23

---
+++ task#23/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число: ");
+int cubes = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cubes){
+ int counter = 0;
+ int length = cubes.Length;
+ while (counter < length){
+ cubes[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+ counter++;
+  }
+}
+
+void PrintArr(int[] collecting){
+ int count = collecting.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(collecting[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cubes+1];
+Cube(arry);
+PrintArr(arry);
